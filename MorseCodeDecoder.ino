e93e072bd61337442044f50b48f126b4d9e8cd5d
Daniel Savaria <dtsavaria+git@gmail.com>
2017-09-25 14:24:16 -0400

    Recognize spaces between letters and words. Closes #1 Closes #3

--- MorseCodeDecoder.ino
+++ MorseCodeDecoder.ino
@@ -9,19 +9,30 @@ const int BUTTON = 7;
 const int NOTHING = 0; //represents no input
 const int DOT = 1; //represent dot input
 const int DASH = 2; //represent dash input
+const int LETTER_SPACE = 3; //represent letter space
+const int WORD_SPACE = 4; //represent word space
 
 unsigned long dotMillisMin = 50; //minimum length of a button press for a dot in milliseconds
 unsigned long dashMillisMin = 200; //minimum length of a button press for a dash in milliseconds
 
+unsigned long letterSpaceMillisMin = 500; //minimum length of no button press for a letter in milliseconds
+unsigned long wordSpaceMillisMin = 1700; //minimum length of no button press for a word space in milliseconds
+
 int inputVal = LOW; //the current input
 int previousInputVal = LOW; //the previous input
+
 unsigned long holdStart = 0; //the time the input started
 unsigned long currentHold = 0; //the length of time of the current button press in milliseconds
 unsigned long previousHold = 0; //the length of time of the previous button press in milliseconds
 
+unsigned long spaceStart = 0; //the time the space started
+unsigned long currentSpace = 0; //the length of time of the current space in milliseconds
+unsigned long previousSpace = 0; //the length of time of the previous space in milliseconds
+
 int determineDotDash(unsigned long holdMillis);
-String getDotDashString(int input);
-String getDotDashSymbolString(int input);
+int determineLetterWordSpace(unsigned long spaceMillis);
+String getDotDashOrSpaceString(int input);
+String getDotDashOrSpaceSymbolString(int input);
 
 void setup() {
   pinMode(LED, OUTPUT);
@@ -38,27 +49,40 @@ void loop() {
     holdStart = millis(); //capture the time the button started being pressed
   }
 
+  if (inputVal == LOW && previousInputVal == HIGH) {
+    spaceStart = millis();
+  }
+
   if (inputVal == HIGH) {
     currentHold = millis() - holdStart; //if the button is being pressed, see for how long
+    currentSpace = 0; //the button is being pressed, reset the space
   } else {
     currentHold = 0; //the button is not being pressed, reset the hold
+    currentSpace = millis() - spaceStart; //the button is not being pressed, see for how long
   }
+
+  int result = NOTHING;
 
   if (currentHold < previousHold) {
 
     //if the button stopped being pressed, determine if the previous press was a dot or dash
-    int result = determineDotDash(previousHold);
+    result = determineDotDash(previousHold);
+  } else if (currentSpace < previousSpace) {
 
+    //if the space is over, determine if the previous space was a letter or word space
+    result = determineLetterWordSpace(previousSpace);
+  }
+
+  if (result != NOTHING) {
     //print the results for now
-    if (result == DOT || DASH) {
-      //Serial.println(getDotDashString(result) + "(" + previousHold + ")");
-      Serial.print(getDotDashSymbolString(result));
-    }
+    Serial.print(getDotDashOrSpaceSymbolString(result));
+    //Serial.println(getDotDashOrSpaceString(result) + "(" + previousHold + ", " + previousSpace + ")");
   }
 
   //set the previous values
   previousInputVal = inputVal;
   previousHold = currentHold;
+  previousSpace = currentSpace;
 }
 
 /**
@@ -75,27 +99,48 @@ int determineDotDash(unsigned long holdMillis) {
 }
 
 /**
- * Given DOT, DASH, or NOTHING, return a string representing it
+ * Given the length of a space in milliseconds, return LETTER_SPACE, WORD_SPACE, or NOTHING
  */
-String getDotDashString(int input) {
-  String toReturn = "nothing";
-  if (input == DOT) {
-    toReturn = "dot";
-  } else if (input == DASH) {
-    toReturn = "dash";
+int determineLetterWordSpace(unsigned long spaceMillis) {
+  int toReturn = NOTHING;
+  if (previousSpace >= wordSpaceMillisMin) {
+    toReturn = WORD_SPACE;
+  } else if (previousSpace >= letterSpaceMillisMin) {
+    toReturn = LETTER_SPACE;
   }
   return toReturn;
 }
 
 /**
- * Given DOT, DASH, or NOTHING, return a string with a symbol
+ * Given DOT, DASH, LETTER_SPACE, WORD_SPACE, or NOTHING, return a string representing it
  */
-String getDotDashSymbolString(int input) {
+String getDotDashOrSpaceString(int input) {
+  String toReturn = "nothing";
+  if (input == DOT) {
+    toReturn = "dot";
+  } else if (input == DASH) {
+    toReturn = "dash";
+  } else if (input == LETTER_SPACE) {
+    toReturn = "end of letter";
+  } else if (input == WORD_SPACE) {
+    toReturn = "end of word";
+  }
+  return toReturn;
+}
+
+/**
+ * Given DOT, DASH, LETTER_SPACE, WORD_SPACE, or NOTHING, return a string with a symbol
+ */
+String getDotDashOrSpaceSymbolString(int input) {
   String toReturn = "";
   if (input == DOT) {
     toReturn = "·";
   } else if (input == DASH) {
     toReturn = "-";
+  } else if (input == LETTER_SPACE) {
+    toReturn = " ";
+  } else if (input == WORD_SPACE) {
+    toReturn = " | ";
   }
   return toReturn;
 }
